a7d3907ac9d53548bddbd11283a674e0308a51a7
Valentin Mladenov <valentin.s.mladenov@gmail.com>
2019-11-25 21:53:07 +0200

    Adding Inquiries Controller and Views.

---
+++ Pharmacy/Pharmacy.WebApp/Controllers/InquiriesController.cs
@@ -0,0 +1,96 @@
+﻿using CsvHelper;
+using Pharmacy.WebApp.Models.DataAccessLayer;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Pharmacy.WebApp.Controllers
+{
+    public class InquiriesController : Controller
+    {
+        private PharmacyDbContext db = new PharmacyDbContext();
+
+        // GET: LowestQuantityMedicines
+        public ActionResult LowestQuantityMedicines()
+        {
+            var medicines = db.Medicines
+                .ToList()
+                .Where(f => f.AvailableQuantity <= 10)
+                .OrderBy(f => f.AvailableQuantity)
+                .ToList();
+
+            ViewBag.CsvString = this.GenerateCsvString(medicines.ToList<object>());
+            return View(medicines);
+        }
+
+        // GET: TopTenMostSoldMedicines
+        public ActionResult TopTenMostSoldMedicines()
+        {
+            var medicines = db.Medicines
+                .OrderBy(f => f.SoldMedicines.Select(sm => sm.Quantity).Sum())
+                .Take(10)
+                .ToList();
+
+            ViewBag.CsvString = this.GenerateCsvString(medicines.ToList<object>());
+            return View(medicines);
+        }
+
+        // GET: TopTenMostProfitableMedicines
+        public ActionResult TopTenMostProfitableMedicines()
+        {
+            var medicines = db.Medicines
+                .OrderBy(m => m.SoldMedicines.Select(sm => sm.Quantity * sm.Price).Sum())
+                .Take(10)
+                .ToList();
+
+            ViewBag.CsvString = this.GenerateCsvString(medicines.ToList<object>());
+            return View(medicines);
+        }
+
+        // GET: TopTenMostProfitableClients
+        public ActionResult TopTenMostProfitableClients()
+        {
+            var persons = db.Persons
+                .OrderBy(p => p.Sales.Select(s => s.SoldMedicines.Select(sm => sm.Quantity * sm.Price).Sum()).Sum())
+                .Take(10)
+                .ToList();
+
+            var csvString = this.GenerateCsvString(persons.ToList<object>());
+            return View(persons);
+        }
+
+        // GET: MedicineLowestPricesWithSupplier
+        public ActionResult MedicineLowestPricesWithSupplier()
+        {
+            var medicines = db.Medicines
+                .OrderBy(m => m.DeliveredMedicines.Min(sm => sm.Price))
+                .ToList();
+
+            var csvString = this.GenerateCsvString(medicines.ToList<object>());
+            return View(medicines);
+        }
+
+        private string GenerateCsvString(List<object> viewModels)
+        {
+            if (viewModels.Count == 0)
+            {
+                return string.Empty;
+            }
+
+            using (var memoryStream = new MemoryStream())
+            {
+                using (var streamWriter = new StreamWriter(memoryStream))
+                using (var csvWriter = new CsvWriter(streamWriter))
+                {
+                    csvWriter.WriteRecords(viewModels);
+                } // StreamWriter gets flushed here.
+
+                return "data:text/csv;charset=utf-8, " + Encoding.Default.GetString(memoryStream.ToArray());
+            }
+        }
+    }
+}
